ef2d237faee2226a6e2f0dc6081040a9f0fed67b
Chris Taylor <theotherchristaylor@gmail.com>
2015-02-22 02:27:40 -0700

    Polished The Crunch.

--- thecrunch.py
+++ thecrunch.py
@@ -1,9 +1,10 @@
 """ 
 thecrunch.py
 
-An attempt to expose the peculiar behaviours of okcupid users. 
+An attempt to expose the peculiar behaviours of lonely people. 
 
-Written by Chris Taylor 2/21/15
+Written by Chris Taylor, 2/21/15
+Inspired by Charles Bukowski, known drunk.
 
 """
 from db.okdatabase import OKDatabase
@@ -22,7 +23,13 @@ total_women = int(total_women[0])
 percent_men = float(total_men)/float(sample_size) * 100
 percent_women = float(total_women)/float(sample_size) * 100
 
-print "%s Lonely souls looking for love." % str(sample_size)
+print """
+###########################
+# 			THE CRUNCH        #
+###########################
+"""
+
+print "%s Lonely souls looking for love./n" % str(sample_size)
 
 print "%.1f%% Men" % percent_men
 print "%.1f%% Women" % percent_women
